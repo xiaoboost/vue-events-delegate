56610ec34c1ddc95cf59ead7d59b884fe91167d3
Xiao <xiaoboost@163.com>
2017-12-16 13:58:47 +0800

    [events] finish class delege event

--- src/events.ts
+++ src/events.ts
@@ -1,7 +1,128 @@
+/** class delegate base event */
+abstract class Delegate {
+    _originalEvent: { [key: string]: any };
+
+    // Event
+    get bubbles(): boolean {
+        return this._originalEvent.bubbles;
+    }
+    get cancelable(): boolean {
+        return this._originalEvent.cancelable;
+    }
+    get cancelBubble(): boolean {
+        return this._originalEvent.cancelBubble;
+    }
+    get composed(): boolean {
+        return this._originalEvent.composed;
+    }
+    get eventPhase(): number {
+        return this._originalEvent.eventPhase;
+    }
+    get isTrusted(): boolean {
+        return this._originalEvent.isTrusted;
+    }
+
+    // MouseEvent
+    get button(): number {
+        return this._originalEvent.button;
+    }
+    get buttons(): number {
+        return this._originalEvent.buttons;
+    }
+    get clientX(): number {
+        return this._originalEvent.clientX;
+    }
+    get clientY(): number {
+        return this._originalEvent.clientY;
+    }
+    get movementX(): number {
+        return this._originalEvent.movementX;
+    }
+    get movementY(): number {
+        return this._originalEvent.movementY;
+    }
+    get offsetX(): number {
+        return this._originalEvent.offsetX;
+    }
+    get offsetY(): number {
+        return this._originalEvent.offsetY;
+    }
+    get pageX(): number {
+        return this._originalEvent.pageX;
+    }
+    get pageY(): number {
+        return this._originalEvent.pageY;
+    }
+    get region(): string {
+        return this._originalEvent.region;
+    }
+    get screenX(): number {
+        return this._originalEvent.screenX;
+    }
+    get screenY(): number {
+        return this._originalEvent.screenY;
+    }
+    get x(): number {
+        return this._originalEvent.x;
+    }
+    get y(): number {
+        return this._originalEvent.y;
+    }
+
+    // WheelEvent
+    get deltaX(): number {
+        return this._originalEvent.deltaX;
+    }
+    get deltaY(): number {
+        return this._originalEvent.deltaY;
+    }
+    get deltaZ(): number {
+        return this._originalEvent.deltaZ;
+    }
+    get deltaMode(): number {
+        return this._originalEvent.deltaMode;
+    }
+
+    // KeyboardEvent
+    get altKey(): boolean {
+        return this._originalEvent.altKey;
+    }
+    get code(): string {
+        return this._originalEvent.code;
+    }
+    get ctrlKey(): boolean {
+        return this._originalEvent.ctrlKey;
+    }
+    get isComposing(): boolean {
+        return this._originalEvent.isComposing;
+    }
+    get key(): string {
+        return this._originalEvent.key;
+    }
+    get locale(): string {
+        return this._originalEvent.locale;
+    }
+    get location(): number {
+        return this._originalEvent.location;
+    }
+    get metaKey(): boolean {
+        return this._originalEvent.metaKey;
+    }
+    get repeat(): boolean {
+        return this._originalEvent.repeat;
+    }
+    get shiftKey(): boolean {
+        return this._originalEvent.shiftKey;
+    }
+
+    // methods
+    getModifierState(keyArg: string): boolean {
+        return this._originalEvent.getModifierState(keyArg);
+    }
+}
 
 /** Delegate Event  */
-export class $Event {
-    _originalEvent: Event;
+export class $Event extends Delegate {
     currentTarget: HTMLElement;
     delegateTarget: HTMLElement;
     target: HTMLElement;
@@ -14,17 +135,10 @@ export class $Event {
     relatedTarget: HTMLElement;
     result: any;
 
-    button: number;
-    key: string;
+    _originalEvent: Event;
 
     constructor(originEvent: Event) {
-        const ans = [];
-        for (const key in originEvent) {
-            ans.push(key);
-        }
-        console.log(ans);
-
-        debugger;
+        super();
         this._originalEvent = originEvent;
     }
 
