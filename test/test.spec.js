272219651253ed3787bb3cd3fbb836b4c2f74d49
Xiao <xiaoboost@163.com>
2018-01-06 16:37:13 +0800

    add test basic usage.

--- test/test.spec.js
+++ test/test.spec.js
@@ -4,10 +4,84 @@ import { destroyVM, createVue } from './utils';
 
 Vue.use(delegate);
 
-describe('null or undefined handler', () => {
-    let vm;
-    afterEach(() => destroyVM(vm));
+let vm;
+afterEach(() => destroyVM(vm));
 
+describe('basic usage', () => {
+    const template = {
+        template: `
+            <div v-delegate:click="['.a', onClickA]">
+                <div>
+                    <div class="a"></div>
+                    <div class="b"></div>
+                </div>
+            </div>`,
+        data() {
+            return {
+                clickA: 0,
+                clickB: 0,
+            };
+        },
+        methods: {
+            onClickA() {
+                this.clickA++;
+            },
+            onClickB() {
+                this.clickB++;
+            },
+        },
+    };
+
+    it('trigger the event and the handler runs', () => {
+        vm = createVue(template);
+
+        const elm = vm.$el.querySelector('.a');
+        // origin event
+        vm.triggerEvent(elm, 'click');
+        expect(vm.clickA).to.equal(1);
+        // delegate event
+        vm.trigger$Event(elm, 'click');
+        expect(vm.clickA).to.equal(2);
+    });
+
+    it('trigger handler that binding by ".delegateOn()"', () => {
+        vm = createVue(template);
+
+        vm.delegateOn({
+            el: vm.$el,
+            type: 'click',
+            selector: '.b',
+            fn: vm.onClickB,
+        });
+
+        const elmA = vm.$el.querySelector('.a');
+        const elmB = vm.$el.querySelector('.b');
+
+        vm.triggerEvent(elmA, 'click');
+        expect(vm.clickA).to.equal(1);
+        expect(vm.clickB).to.equal(0);
+
+        vm.triggerEvent(elmB, 'click');
+        expect(vm.clickA).to.equal(1);
+        expect(vm.clickB).to.equal(1);
+    });
+
+    it('handler should not run after remove handler', () => {
+        vm = createVue(template);
+
+        const elm = vm.$el.querySelector('.a');
+
+        vm.triggerEvent(elm, 'click');
+        expect(vm.clickA).to.equal(1);
+
+        vm.delegateOff(vm.$el);
+
+        vm.triggerEvent(elm, 'click');
+        expect(vm.clickA).to.equal(1);
+    });
+});
+
+describe('null or undefined handler', () => {
     it('Binding a null handler in template will throw an exception', () => {
         try {
             vm = createVue({ template: '<div v-delegate:click="null"></div>' });
@@ -25,60 +99,3 @@ describe('null or undefined handler', () => {
         }
     });
 });
-
-// describe('base function', () => {
-//     let vm;
-//     afterEach(() => destroyVM(vm));
-
-//     // describe('mouse event', () => {
-//     //     vm = createVue({
-//     //         template:
-//     //             `<div
-//     //                 class="outter"
-//     //                 v-delegate:click-a="['.text1', clickHandler1]"
-//     //                 v-delegate:click-b="['.text2', clickHandler2]"
-//     //                 v-delegate:mouseenter="['.inner', mouseenterHandler]"
-//     //                 v-delegate:mouseleave="['.inner', mouseleaveHandler]">
-//     //                 <div class="inner">
-//     //                     <span class="text1">第一个文本</span>
-//     //                     <span class="text2">第二个文本</span>
-//     //                 </div>
-//     //             </div>`,
-
-//     //         data() {
-//     //             return {
-//     //                 textClick01: 0,
-//     //                 textClick02: 0,
-//     //                 mouseenter: 0,
-//     //                 mouseleave: 0,
-//     //             };
-//     //         },
-//     //         methods: {
-//     //             clickHandler1() {
-//     //                 this.textClick01++;
-//     //             },
-//     //             clickHandler2() {
-//     //                 this.textClick02++;
-//     //             },
-//     //             mouseenterHandler() {
-//     //                 this.mouseenter++;
-//     //             },
-//     //             mouseleaveHandler() {
-//     //                 this.mouseleave++;
-//     //             },
-//     //         },
-//     //     });
-
-//     //     it('click event', () => {
-//     //         const text1 = vm.$el.querySelector('.text1');
-//     //         // const text2 = vm.$el.querySelector('.text2');
-
-//     //         vm.triggerEvent(text1, 'click');
-//     //         expect(vm.textClick01).to.equal(1);
-//     //         expect(vm.textClick02).to.equal(0);
-//     //         // triggerEvent(text2, 'click');
-//     //         // expect(vm.textClick01).to.equal(1);
-//     //         // expect(vm.textClick02).to.equal(1);
-//     //     });
-//     // });
-// });
